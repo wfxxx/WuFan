d779cba4fc644b5f4df60eaa37485b7a6dbe9294
Legolas <weiguo.wu@definesys.com>
2020-02-11 23:32:37 +0800

    初始化项目

---
+++ src/main/java/com/definesys/dsgc/service/DsgcappservicesApplication.java
@@ -0,0 +1,13 @@
+package com.definesys.dsgc.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsgcappservicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DsgcappservicesApplication.class,args);
+    }
+
+}
---
+++ src/test/java/com/definesys/dsgc/service/DsgcappservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.definesys.dsgc.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsgcappservicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
