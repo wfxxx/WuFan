bbb969728deb115bb57605642a489b2e43914372
薛云龙 <yunlong.xue@definesys.com>
2020-02-17 16:04:38 +0800

    获取wsdl完整地址的部分逻辑

---
+++ src/test/java/com/definesys/dsgc/service/svcmng/SVCMngServiceTest.java
@@ -0,0 +1,34 @@
+package com.definesys.dsgc.service.svcmng;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.junit.Assert.*;
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
+public class SVCMngServiceTest {
+
+    @Autowired
+    SVCMngService svcMngService;
+
+
+
+    @Test
+    public void getWsdlBodyReq() {
+        String wsdlUrl="http://www.webxml.com.cn/WebServices/IpAddressSearchWebService.asmx?wsdl";
+        String method="getCountryCityByIp";
+        String result =svcMngService.getWsdlBodyReq(wsdlUrl,method);
+        System.out.println(result);
+    }
+
+    @Test
+    public void getWsdlBodyRes() {
+        String wsdlUrl="http://www.webxml.com.cn/WebServices/IpAddressSearchWebService.asmx?wsdl";
+        String method="getCountryCityByIp";
+        String result =svcMngService.getWsdlBodyRes(wsdlUrl,method);
+        System.out.println(result);
+    }
+}
