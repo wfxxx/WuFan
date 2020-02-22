d7d1695256decdc5f6a7e9d493c0c5a0a4fc3947
zini.ye <zini.ye@definesys.com>
2020-02-22 10:23:49 +0800

    负载均衡模块

--- src/main/java/com/definesys/dsgc/service/apilr/ApiLrDao.java
+++ src/main/java/com/definesys/dsgc/service/apilr/ApiLrDao.java
@@ -1,7 +1,68 @@
 package com.definesys.dsgc.service.apilr;
 
+import com.definesys.dsgc.service.apilr.bean.CommonReqBean;
+import com.definesys.dsgc.service.apilr.bean.DagLrbean;
+import com.definesys.dsgc.service.system.bean.DSGCSystemUser;
+import com.definesys.dsgc.service.utils.StringUtil;
+import com.definesys.mpaas.query.MpaasQuery;
+import com.definesys.mpaas.query.MpaasQueryFactory;
+import com.definesys.mpaas.query.db.PageQueryResult;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public class ApiLrDao {
+    @Autowired
+    private MpaasQueryFactory sw;
+
+    public List<DSGCSystemUser> findUserSystemByUserId(String userId){
+        return sw.buildQuery().eq("user_id",userId).doQuery(DSGCSystemUser.class);
+    }
+
+    public PageQueryResult queryApiLrList(CommonReqBean param, int pageSize, int pageIndex, String userRole, List<String> sysCodeList){
+        StringBuffer sqlStr = new StringBuffer("select db.*,dse.sys_name appName from DAG_LR db,dsgc_system_entities dse where 1=1 and db.app_code = dse.sys_code ");
+        MpaasQuery mq = sw.buildQuery();
+        if ("SystemLeader".equals(userRole)) {
+            sqlStr.append(" and db.app_code in ( ");
+            for (int i = 0; i < sysCodeList.size(); i++) {
+                if (i < sysCodeList.size() - 1) {
+                    sqlStr.append("'" + sysCodeList.get(i) + "',");
+                } else {
+                    sqlStr.append("'" + sysCodeList.get(i) + "') ");
+                }
+            }
+        }
+        if (StringUtil.isNotBlank(param.getCon0())) {
+            String[] conArray = param.getCon0().trim().split(" ");
+            for (String s : conArray) {
+                if (s != null && s.length() > 0) {
+                    sqlStr.append(this.generateLikeAndCluse(s));
+                }
+            }
+        }
+        mq.sql(sqlStr.toString());
+        return mq.doPageQuery(pageIndex, pageSize, DagLrbean.class);
+    }
+
+    private String generateLikeAndCluse(String con) {
+        String conUpper = con.toUpperCase();
+        String conAnd = " and  (UPPER(db.lr_name) like '%" + conUpper + "%'";
+        conAnd += " or UPPER(db.lr_desc) like '%" + conUpper + "%'";
+        conAnd += " or UPPER(dse.sys_name) like '%" + conUpper + "%' )";
+        return conAnd;
+    }
+
+    public void addApiLr(DagLrbean dagLrbean){
+        sw.buildQuery().doInsert(dagLrbean);
+    }
+
+    public void delApiBs(CommonReqBean param){
+        sw.buildQuery().eq("dl_id",param.getCon0()).doDelete(DagLrbean.class);
+    }
+
+    public DagLrbean queryApiLrById(CommonReqBean param){
+        return sw.buildQuery().eq("dl_id",param.getCon0()).doQueryFirst(DagLrbean.class);
+    }
 }
---
+++ src/main/java/com/definesys/dsgc/service/apilr/bean/DagLrbean.java
@@ -0,0 +1,122 @@
+package com.definesys.dsgc.service.apilr.bean;
+
+import com.definesys.mpaas.query.annotation.*;
+import com.definesys.mpaas.query.json.MpaasDateTimeDeserializer;
+import com.definesys.mpaas.query.json.MpaasDateTimeSerializer;
+import com.definesys.mpaas.query.model.MpaasBasePojo;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+
+import java.io.Serializable;
+import java.util.Date;
+
+@Table(value = "DAG_LR")
+public class DagLrbean extends MpaasBasePojo implements Serializable {
+    @RowID(type = RowIDType.UUID)
+    private String dlId;
+
+    private String lrName;
+    private String lrDesc;
+    private String appCode;
+    @Column(type = ColumnType.JAVA)
+    private String appName;
+    @SystemColumn(SystemColumnType.CREATE_BY)
+    @Column(value = "created_by")
+    private String createdBy;
+    @JsonSerialize(using = MpaasDateTimeSerializer.class)
+    @JsonDeserialize(using = MpaasDateTimeDeserializer.class)
+    @SystemColumn(SystemColumnType.CREATE_ON)
+    private Date creationDate;
+    @SystemColumn(SystemColumnType.LASTUPDATE_BY)
+    @Column(value = "last_updated_by")
+    private String lastUpdatedBy;
+
+    @JsonSerialize(using = MpaasDateTimeSerializer.class)
+    @JsonDeserialize(using = MpaasDateTimeDeserializer.class)
+    @SystemColumn(SystemColumnType.LASTUPDATE_ON)
+    @Column(value = "last_update_date")
+    private Date lastUpdateDate;
+    @SystemColumn(SystemColumnType.OBJECT_VERSION)
+    @Column(value = "object_version_number")
+    private Integer objectVersionNumber;
+
+    public String getDlId() {
+        return dlId;
+    }
+
+    public void setDlId(String dlId) {
+        this.dlId = dlId;
+    }
+
+    public String getLrName() {
+        return lrName;
+    }
+
+    public void setLrName(String lrName) {
+        this.lrName = lrName;
+    }
+
+    public String getLrDesc() {
+        return lrDesc;
+    }
+
+    public void setLrDesc(String lrDesc) {
+        this.lrDesc = lrDesc;
+    }
+
+    public String getAppCode() {
+        return appCode;
+    }
+
+    public void setAppCode(String appCode) {
+        this.appCode = appCode;
+    }
+
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public String getLastUpdatedBy() {
+        return lastUpdatedBy;
+    }
+
+    public void setLastUpdatedBy(String lastUpdatedBy) {
+        this.lastUpdatedBy = lastUpdatedBy;
+    }
+
+    public Date getLastUpdateDate() {
+        return lastUpdateDate;
+    }
+
+    public void setLastUpdateDate(Date lastUpdateDate) {
+        this.lastUpdateDate = lastUpdateDate;
+    }
+
+    public Integer getObjectVersionNumber() {
+        return objectVersionNumber;
+    }
+
+    public void setObjectVersionNumber(Integer objectVersionNumber) {
+        this.objectVersionNumber = objectVersionNumber;
+    }
+
+    public String getAppName() {
+        return appName;
+    }
+
+    public void setAppName(String appName) {
+        this.appName = appName;
+    }
+}
