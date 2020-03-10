b8af641a2e723cb32cd90635b6afccc864c3d405
zini.ye <zini.ye@definesys.com>
2020-03-10 15:41:35 +0800

    权限控制

--- src/main/java/com/definesys/dsgc/service/users/bean/CheckSysLeaderRoleVO.java
+++ src/main/java/com/definesys/dsgc/service/users/bean/CheckSysLeaderRoleVO.java
@@ -3,6 +3,7 @@ package com.definesys.dsgc.service.users.bean;
 public class CheckSysLeaderRoleVO {
     private String userId;
     private String servNo;
+    private String apiCode;
 
     public String getUserId() {
         return userId;
@@ -18,5 +19,13 @@ public class CheckSysLeaderRoleVO {
 
     public void setServNo(String servNo) {
         this.servNo = servNo;
+    }
+
+    public String getApiCode() {
+        return apiCode;
+    }
+
+    public void setApiCode(String apiCode) {
+        this.apiCode = apiCode;
     }
 }
