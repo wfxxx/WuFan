daa653c9fd99a9680abe9336e9690a5ebb777f56
薛云龙 <yunlong.xue@definesys.com>
2020-03-17 10:08:59 +0800

    插件列表后端

--- src/main/java/com/definesys/dsgc/service/apiplugin/ApiPlugInService.java
+++ src/main/java/com/definesys/dsgc/service/apiplugin/ApiPlugInService.java
@@ -1,10 +1,51 @@
 package com.definesys.dsgc.service.apiplugin;
 
+import com.definesys.dsgc.service.apilr.ApiLrDao;
+import com.definesys.dsgc.service.apilr.bean.CommonReqBean;
+import com.definesys.dsgc.service.apilr.bean.DagLrListDTO;
+import com.definesys.dsgc.service.apilr.bean.DagLrbean;
+import com.definesys.dsgc.service.apiplugin.bean.DAGPluginListVO;
+import com.definesys.dsgc.service.system.bean.DSGCSystemUser;
+import com.definesys.mpaas.query.db.PageQueryResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 
 @Service
 public class ApiPlugInService {
     @Autowired
     private ApiPlugInDao apiPlugInDao;
+    @Autowired
+    private ApiLrDao apiLrDao;
+
+
+    public PageQueryResult queryPluginList(CommonReqBean param, String userId, String userRole, int pageSize, int pageIndex ){
+        List<String> sysCodeList = new ArrayList<>();
+        if ("SystemLeader".equals(userRole)){
+            List<DSGCSystemUser> dsgcSystemUsers =   apiLrDao.findUserSystemByUserId(userId);
+            Iterator<DSGCSystemUser> iter = dsgcSystemUsers.iterator();
+            while (iter.hasNext()) {
+                DSGCSystemUser s = (DSGCSystemUser) iter.next();
+                sysCodeList.add(s.getSysCode());
+            }
+        }
+        PageQueryResult result=apiPlugInDao.queryPluginList(param,pageSize,pageIndex,userRole,sysCodeList);
+        List<DAGPluginListVO> queryPluginList= result.getResult();
+        for(DAGPluginListVO item:queryPluginList){
+            Map<String,Object> value=apiPlugInDao.queryDeplogDev(item.getVid());
+            if(value !=null){
+                Object devName=value.get("DEV");
+                item.setDevName(devName.toString());
+            }else{
+                item.setDevName("未部署");
+            }
+        }
+        result.setResult(queryPluginList);
+        return  result;
+    }
 }
---
+++ src/main/java/com/definesys/dsgc/service/apiplugin/bean/DAGPluginListVO.java
@@ -0,0 +1,148 @@
+package com.definesys.dsgc.service.apiplugin.bean;
+
+import com.definesys.mpaas.query.annotation.Column;
+import com.definesys.mpaas.query.annotation.SystemColumn;
+import com.definesys.mpaas.query.annotation.SystemColumnType;
+import com.definesys.mpaas.query.json.MpaasDateTimeDeserializer;
+import com.definesys.mpaas.query.json.MpaasDateTimeSerializer;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+
+import java.util.Date;
+
+/**
+ * @ClassName DAGPluginListVO
+ * @Description TODO
+ * @Author Xueyunlong
+ * @Date 2020-3-16 13:21
+ * @Version 1.0
+ **/
+public class DAGPluginListVO {
+
+    //插件使用id
+    private String pluginUsingId;
+    //插件类型
+    private String pluginCode;
+    //插件是否启用
+    private String isEnable;
+    //插件名称
+    private String pluginName;
+    //插件所属配置的id
+    private String vid;
+    //插件所属配置的名称
+    private String configName;
+    //插件所属服务的code
+    private String sourCode;
+    //插件所属服务的类型
+    private String sourType;
+    //插件所属应用的code
+    private String appCode;
+    //插件所属应用的名称
+    private String appName;
+    //插件部署环境
+    private String devName;
+
+    @JsonSerialize(using = MpaasDateTimeSerializer.class)
+    @JsonDeserialize(using = MpaasDateTimeDeserializer.class)
+    @SystemColumn(SystemColumnType.CREATE_ON)
+    private Date creationDate;
+
+
+    public String getPluginUsingId() {
+        return pluginUsingId;
+    }
+
+    public void setPluginUsingId(String pluginUsingId) {
+        this.pluginUsingId = pluginUsingId;
+    }
+
+    public String getPluginCode() {
+        return pluginCode;
+    }
+
+    public void setPluginCode(String pluginCode) {
+        this.pluginCode = pluginCode;
+    }
+
+    public String getIsEnable() {
+        return isEnable;
+    }
+
+    public void setIsEnable(String isEnable) {
+        this.isEnable = isEnable;
+    }
+
+    public String getPluginName() {
+        return pluginName;
+    }
+
+    public void setPluginName(String pluginName) {
+        this.pluginName = pluginName;
+    }
+
+    public String getSourType() {
+        return sourType;
+    }
+
+    public void setSourType(String sourType) {
+        this.sourType = sourType;
+    }
+
+
+
+    public String getVid() {
+        return vid;
+    }
+
+    public void setVid(String vid) {
+        this.vid = vid;
+    }
+
+    public String getConfigName() {
+        return configName;
+    }
+
+    public void setConfigName(String configName) {
+        this.configName = configName;
+    }
+
+    public String getSourCode() {
+        return sourCode;
+    }
+
+    public void setSourCode(String sourCode) {
+        this.sourCode = sourCode;
+    }
+
+    public String getAppCode() {
+        return appCode;
+    }
+
+    public void setAppCode(String appCode) {
+        this.appCode = appCode;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public String getAppName() {
+        return appName;
+    }
+
+    public void setAppName(String appName) {
+        this.appName = appName;
+    }
+
+    public String getDevName() {
+        return devName;
+    }
+
+    public void setDevName(String devName) {
+        this.devName = devName;
+    }
+}
